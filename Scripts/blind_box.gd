ba09b36843910df24cf9a4dbaf6644c82b2d87b7
PANDA-NR200P\azure <ashwong1994@gmail.com>
2026-07-07 09:15:24 -0700

    July 6 Updated Animations when opening box

--- Scripts/blind_box.gd
+++ Scripts/blind_box.gd
@@ -2,29 +2,49 @@ extends Control
 
 @onready var popup2 = $You_Got 
 @onready var openbox = $OpenBox
+
 @onready var placeholder_openbox = $spr_PlaceholderOpenBox
-@onready var heart_openbox = $spr_HeartOpenBox
+
+@onready var heart_openbox = $spr_HeartOpenBox_Common
+@onready var brain_openbox = $spr_BrainOpenBox_Common
+@onready var kidney_openbox = $spr_KidneyOpenBox_Common
+@onready var liver_openbox = $spr_LiverOpenBox_Common
+@onready var lungs_openbox = $spr_LungsOpenBox_Common
+@onready var sml_intestine_openbox = $spr_SmlIntesOpenBox_Common
+@onready var lrg_intestine_openbox = $spr_LrgIntesOpenBox_Common
+@onready var stomach_openbox = $spr_StomachOpenBox_Common
+
+@onready var heart_openbox_rare = $spr_HeartOpenBox_Rare
+@onready var brain_openbox_rare = $spr_BrainOpenBox_Rare
+@onready var kidney_openbox_rare = $spr_KidneyOpenBox_Rare
+@onready var liver_openbox_rare = $spr_LiverOpenBox_Rare
+@onready var lungs_openbox_rare = $spr_LungsOpenBox_Rare
+@onready var sml_intestine_openbox_rare = $spr_SmlIntesOpenBox_Rare
+@onready var lrg_intestine_openbox_rare = $spr_LrgIntesOpenBox_Rare
+@onready var stomach_openbox_rare = $spr_StomachOpenBox_Rare
+
 @onready var show_prize = $You_Got/show_prize
 
 # We can store the reward data cleanly in a Dictionary structure
-var rewards = {
-	1:  {"global_var": "has_brain", "texture": "res://Assets/Trading Cards/F_Basic_Heart.png", "is_heart": false}, # Wait, your original code assigned get_Brain here but set_Heart texture? Double check your paths!
-	2:  {"global_var": "has_heart", "texture": "res://Assets/Trading Cards/F_Basic_Heart.png", "is_heart": true},
-	3:  {"global_var": "has_kidneys", "texture": "res://Assets/Trading Cards/F_Basic_Kidney.png", "is_heart": false},
-	4:  {"global_var": "has_liver", "texture": "res://Assets/Trading Cards/F_Basic_Liver.png", "is_heart": false},
-	5:  {"global_var": "has_largeintestines", "texture": "res://Assets/Trading Cards/F_Basic_LargeIntestine.png", "is_heart": false},
-	6:  {"global_var": "has_smallintestines", "texture": "res://Assets/Trading Cards/F_Basic_SmallIntestines.png", "is_heart": false},
-	7:  {"global_var": "has_lungs", "texture": "res://Assets/Trading Cards/F_Basic_Lung.png", "is_heart": false},
-	8:  {"global_var": "has_stomach", "texture": "res://Assets/Trading Cards/F_Basic_Stomach.png", "is_heart": false},
+# Add @onready here so it can reference the sprite nodes below
+@onready var rewards = {
+	1:  {"node": brain_openbox, "global_var": "has_brain", "texture": "res://Assets/Trading Cards/F_Basic_Heart.png"}, # Wait, your original code assigned get_Brain here but set_Heart texture? Double check your paths!
+	2:  {"node": heart_openbox, "global_var": "has_heart", "texture": "res://Assets/Trading Cards/F_Basic_Heart.png"},
+	3:  {"node": kidney_openbox, "global_var": "has_kidneys", "texture": "res://Assets/Trading Cards/F_Basic_Kidney.png"},
+	4:  {"node": liver_openbox, "global_var": "has_liver", "texture": "res://Assets/Trading Cards/F_Basic_Liver.png"},
+	5:  {"node": lrg_intestine_openbox, "global_var": "has_largeintestines", "texture": "res://Assets/Trading Cards/F_Basic_LargeIntestine.png"},
+	6:  {"node": sml_intestine_openbox, "global_var": "has_smallintestines", "texture": "res://Assets/Trading Cards/F_Basic_SmallIntestines.png"},
+	7:  {"node": lungs_openbox, "global_var": "has_lungs", "texture": "res://Assets/Trading Cards/F_Basic_Lung.png"},
+	8:  {"node": stomach_openbox, "global_var": "has_stomach", "texture": "res://Assets/Trading Cards/F_Basic_Stomach.png"},
 	# Rare Organs
-	9:  {"global_var": "has_rare_brain", "texture": "res://Assets/Trading Cards/F_Rare_Brain.png", "is_heart": false},
-	10: {"global_var": "has_rare_heart", "texture": "res://Assets/Trading Cards/F_Rare_Heart.png", "is_heart": false},
-	11: {"global_var": "has_rare_kidneys", "texture": "res://Assets/Trading Cards/F_Rare_Kidney.png", "is_heart": false},
-	12: {"global_var": "has_rare_liver", "texture": "res://Assets/Trading Cards/F_Rare_Liver.png", "is_heart": false},
-	13: {"global_var": "has_rare_largeintestines", "texture": "res://Assets/Trading Cards/F_Rare_LargeIntestine.png", "is_heart": false},
-	14: {"global_var": "has_rare_smallintestines", "texture": "res://Assets/Trading Cards/F_Rare_SmallIntestines.png", "is_heart": false},
-	15: {"global_var": "has_rare_lungs", "texture": "res://Assets/Trading Cards/F_Rare_Lung.png", "is_heart": false},
-	16: {"global_var": "has_rare_stomach", "texture": "res://Assets/Trading Cards/F_Rare_Stomach.png", "is_heart": false}
+	9:  {"node": brain_openbox_rare, "global_var": "has_rare_brain", "texture": "res://Assets/Trading Cards/F_Rare_Brain.png"},
+	10: {"node": heart_openbox_rare, "global_var": "has_rare_heart", "texture": "res://Assets/Trading Cards/F_Rare_Heart.png"},
+	11: {"node": kidney_openbox_rare, "global_var": "has_rare_kidneys", "texture": "res://Assets/Trading Cards/F_Rare_Kidney.png"},
+	12: {"node": liver_openbox_rare, "global_var": "has_rare_liver", "texture": "res://Assets/Trading Cards/F_Rare_Liver.png"},
+	13: {"node": lrg_intestine_openbox_rare, "global_var": "has_rare_largeintestines", "texture": "res://Assets/Trading Cards/F_Rare_LargeIntestine.png"},
+	14: {"node": sml_intestine_openbox_rare, "global_var": "has_rare_smallintestines", "texture": "res://Assets/Trading Cards/F_Rare_SmallIntestines.png"},
+	15: {"node": lungs_openbox_rare, "global_var": "has_rare_lungs", "texture": "res://Assets/Trading Cards/F_Rare_Lung.png"},
+	16: {"node": stomach_openbox_rare, "global_var": "has_rare_stomach", "texture": "res://Assets/Trading Cards/F_Rare_Stomach.png"}
 }
 
 var rng = RandomNumberGenerator.new()
@@ -45,6 +65,7 @@ func _on_change_scene_requested(target: String) -> void:
 
 
 func _on_open_box_pressed() -> void:
+	$OpenBox.hide()
 	if GlobalVariables.tokens <= 0: 
 		return # Stop execution early if they don't have enough tokens
 		
@@ -55,7 +76,7 @@ func _on_open_box_pressed() -> void:
 	var prize_data = rewards[roll]
 	
 	# Determine which sprite node to play based on the reward data
-	var active_anim = heart_openbox if prize_data["is_heart"] else placeholder_openbox
+	var active_anim = prize_data["node"]
 	
 	# Safeguard: Ensure the animations start clean
 	placeholder_openbox.hide()
@@ -81,4 +102,5 @@ func _on_open_box_pressed() -> void:
 
 
 func _on_show_prize_pressed() -> void:
+	$OpenBox.show()
 	popup2.hide()
